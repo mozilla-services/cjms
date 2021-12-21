82f342404039396e58a8ca4bb88156a14f7e37cd
Sarah Bird <birdsarah@mozilla.com>
2021-12-21 15:56:20 -0600

    Add README and failing test for heartbeat

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # cjms
 Micro-service supporting VPN activities
+
+## Developer docs
+
+### Pre-requisites
+
+* Rust - https://www.rust-lang.org/tools/install
+
+Coming soon:
+* Postgres
+
+### Run and test
+
+Optionally set environment variables with tha HOST and PORT
+
+`cargo run`
+
+Will start server running at http://127.0.0.1:8080
+
+`cargo test`
+
+Will run the tests. Integration tests go under tests folder, unit tests go into associated files under src.
+
+### Deployment
+
+Service is deployed using docker containers. 
+
+Install docker and look at github actions for the docker build script.
--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,19 +1,31 @@
-use cjms::handlers;
-
 #[cfg(test)]
 mod tests {
-    use super::*;
-    use actix_web::{test, web::{self, Bytes}, App};
+    use actix_web::{test, App, web::Bytes};
+    use cjms::appconfig::config_app;
+
+    macro_rules! setup_app {
+    () => {
+        test::init_service(
+            App::new()
+            .configure(config_app)
+        )
+        .await
+    };
+}
 
     #[actix_rt::test]
     async fn test_index_get() {
-        let mut app = test::init_service(
-            App::new()
-                .route("/", web::get().to(handlers::index)),
-        )
-        .await;
+        let mut app = setup_app!();
         let req = test::TestRequest::get().uri("/").to_request();
-        let result = test::read_response(&mut app, req).await;
-        assert_eq!(result, Bytes::from_static(b"Hello world!"));
+        let resp = test::read_response(&mut app, req).await;
+        assert_eq!(resp, Bytes::from_static(b"Hello world!"));
+    }
+
+    #[actix_rt::test]
+    async fn test_heartbeat_get() {
+        let mut app = setup_app!();
+        let req = test::TestRequest::get().uri("/__heartbeat__").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(resp.status(), 200);
     }
 }
