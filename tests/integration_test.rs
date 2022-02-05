568bcc4fb1068842b902ee0d4512c96d9f16c2bb
Jon Buckley <jbuckley@mozilla.com>
2022-02-05 11:34:11 -0500

    Add /__lbheartbeat__ handler (#9)

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -28,4 +28,12 @@ mod tests {
         let resp = test::call_service(&mut app, req).await;
         assert_eq!(resp.status(), 200);
     }
+
+    #[actix_rt::test]
+    async fn test_lbheartbeat_get() {
+        let mut app = setup_app!();
+        let req = test::TestRequest::get().uri("/__lbheartbeat__").to_request();
+        let resp = test::call_service(&mut app, req).await;
+        assert_eq!(resp.status(), 200);
+    }
 }
