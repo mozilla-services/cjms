45258ef0f945c98fa2db335bdde1fa34f9efb26f
Sarah Bird <birdsarah@mozilla.com>
2022-02-23 19:12:17 -0600

    Initial switch to config

--- src/bin/cjms.rs
+++ src/bin/cjms.rs
@@ -1,12 +1,12 @@
 use actix_web::{middleware, App, HttpServer};
 
 use cjms::appconfig::config_app;
-use cjms::env::get_env;
+use cjms::settings::get_settings;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let env = get_env();
-    let addr = format!("{}:{}", env.host, env.port);
+    let settings = get_settings(None);
+    let addr = settings.server_address();
     println!("Server running at http://{}", addr);
     HttpServer::new(|| {
         App::new()
---
+++ src/settings.rs
@@ -0,0 +1,78 @@
+use config::{Config, Environment, File, FileFormat};
+
+#[derive(serde::Deserialize, PartialEq, Eq, Debug)]
+pub struct Settings {
+    pub host: String,
+    pub port: String,
+}
+
+impl Settings {
+    pub fn server_address(&self) -> String {
+        format!("{}:{}", self.host, self.port)
+    }
+}
+
+pub fn get_settings(config_file: Option<String>) -> Settings {
+    let mut builder = Config::builder();
+    // Either we use a config file, or we use environment variables
+/*     builder = match config_file {
+        Some(filename) => builder.add_source(File::new(&filename, FileFormat::Yaml)),
+        None => builder.add_source(Environment::default()),
+    }; */
+    builder = builder.add_source(Environment::default());
+    let config = match builder.build() {
+        Ok(config) => config,
+        Err(e) => {
+            panic!("Aborting. Config couldn't be built. {:?}", e)
+        }
+    };
+    match config.try_deserialize::<Settings>() {
+        Ok(settings) => {
+            println!("settings are: {:?}", settings);
+            settings
+        }
+        Err(e) => panic!("Aborting. Config didn't match serialization. {:?}", e),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serial_test::serial;
+    use std::env;
+
+    #[test]
+    #[serial]
+    #[should_panic]
+    fn test_get_settings_missing_envvars() {
+        // If you have set environment variables, this test may fail
+        let _ = get_settings(None);
+    }
+
+    #[test]
+    #[serial]
+    fn test_get_settings_with_envvars() {
+        env::set_var("HOST", "111.2.3.6");
+        env::set_var("PORT", "2222");
+        let actual = get_settings(None);
+        let expected = Settings {
+            host: "111.2.3.6".to_string(),
+            port: "2222".to_string(),
+        };
+        assert_eq!(expected, actual);
+        env::remove_var("HOST");
+        env::remove_var("PORT");
+    }
+
+    #[test]
+    #[serial]
+    fn test_server_address() {
+        env::set_var("HOST", "111.2.3.5");
+        env::set_var("PORT", "2222");
+        let settings = get_settings(None);
+        let actual = settings.server_address();
+        assert_eq!("111.2.3.5:2222", actual);
+        env::remove_var("HOST");
+        env::remove_var("PORT");
+    }
+}
