9116383346f2603bfa87cf7e764aca980774c1c4
Sarah Bird <birdsarah@mozilla.com>
2022-02-25 00:09:22 -0600

    Add migrations

--- migrations/20220224215815_aic_initial_columns.up.sql
+++ migrations/20220224215815_aic_initial_columns.up.sql
@@ -1,1 +1,9 @@
 -- Add up migration script here
+	CREATE TABLE aic (
+	id uuid NOT NULL,
+	PRIMARY KEY (id),
+	cj_event_value TEXT NOT NULL,
+	flow_id TEXT NOT NULL,
+	created timestamptz NOT NULL
+	expires timestamptz NOT NULL
+);
