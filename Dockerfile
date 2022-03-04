af3f9350efd66d18f2cc7c021a5188dea82e8b2f
Sarah Bird <birdsarah@mozilla.com>
2022-03-03 21:22:58 -0600

    Save version.yaml in dockerfile

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM rust:1.57 as build
 WORKDIR /app
 COPY . /app
 RUN cargo build --release
-RUN ./app/target/release/make_version_file
+RUN ./target/release/make_version_file
 
 # Note: If you need to debug this image add ":debug" to the end of the next line
 # https://github.com/GoogleContainerTools/distroless/blob/main/README.md#debug-images
--- src/bin/version.rs
+++ src/bin/version.rs
@@ -1,4 +1,17 @@
+use cjms::controllers::custodial::VERSION_FILE;
+use std::fs;
+use std::str::from_utf8;
+use std::process::Command;
+
 fn main() -> std::io::Result<()> {
-    println!("Version binary");
+    let output = Command::new("git")
+        .arg("rev-parse")
+        .arg("--short")
+        .arg("HEAD")
+        .output()
+        .expect("failed to execute process");
+    let result = from_utf8(&output.stdout).expect("Failed to read output.");
+    println!("{:?}", result);
+    fs::write(VERSION_FILE, "test").ok();
     Ok(())
 }
