757028abf0e722c016abe9936bf98a2a5593c61f
Sarah Bird <birdsarah@mozilla.com>
2022-03-03 22:40:08 -0600

    Add writing version file

--- src/bin/version.rs
+++ src/bin/version.rs
@@ -1,17 +1,38 @@
 use cjms::controllers::custodial::VERSION_FILE;
 use std::fs;
-use std::str::from_utf8;
 use std::process::Command;
+use std::str;
 
 fn main() -> std::io::Result<()> {
-    let output = Command::new("git")
+    // Repo link
+    let source = env!("CARGO_PKG_REPOSITORY");
+
+    // Commit SHA
+    let rev_parse_out = Command::new("git")
         .arg("rev-parse")
         .arg("--short")
         .arg("HEAD")
         .output()
-        .expect("failed to execute process");
-    let result = from_utf8(&output.stdout).expect("Failed to read output.");
-    println!("{:?}", result);
-    fs::write(VERSION_FILE, "test").ok();
+        .expect("Failed to execute git rev-parse");
+    let rev_parse =
+        str::trim(str::from_utf8(&rev_parse_out.stdout).expect("Failed to read output."));
+
+    // Version
+    let tags_out = Command::new("git")
+        .arg("describe")
+        .arg("--tags")
+        .output()
+        .expect("Failed to execute git rev-parse");
+    let tag = str::trim(str::from_utf8(&tags_out.stdout).expect("Failed to read output."));
+
+    fs::write(
+        VERSION_FILE,
+        format!(
+            "source: {}\ncommit: {}\nversion: {}\n",
+            source, rev_parse, tag
+        ),
+    )
+    .expect("Failed to write file.");
+
     Ok(())
 }
