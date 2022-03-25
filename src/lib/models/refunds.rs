c42bc61c2ed773a1229c78c00faf27dc552c21c1
Sarah Bird <birdsarah@mozilla.com>
2022-03-25 17:37:20 -0500

    Add test for refund creation

--- src/lib/models/refunds.rs
+++ src/lib/models/refunds.rs
@@ -3,19 +3,100 @@ use sqlx::{query_as, Error, PgPool};
 use time::OffsetDateTime;
 use uuid::Uuid;
 
+use super::status_history::{Status, UpdateStatus};
+
+pub struct PartialRefund {
+    pub id: Uuid,
+    pub refund_id: String,
+    pub subscription_id: String,
+    pub refund_created: OffsetDateTime,
+    pub refund_amount: i32,
+    pub refund_status: Option<String>,
+    pub refund_reason: Option<String>,
+}
 #[derive(Debug)]
 pub struct Refund {
-    id: Uuid,
-    refund_id: String,
-    subscription_id: String,
-    refund_created: OffsetDateTime,
-    refund_amount: i32,
-    refund_status: Option<String>,
-    refund_reason: Option<String>,
+    pub id: Uuid,
+    pub refund_id: String,
+    pub subscription_id: String,
+    pub refund_created: OffsetDateTime,
+    pub refund_amount: i32,
+    pub refund_status: Option<String>,
+    pub refund_reason: Option<String>,
     // Note we use string and json to save in database for simplicity
     status: Option<String>,
     status_t: Option<OffsetDateTime>,
     status_history: Option<JsonValue>,
+}
+
+impl PartialEq for Refund {
+    fn eq(&self, other: &Self) -> bool {
+        let simple_match = self.id == other.id &&
+        self.refund_id == other.refund_id &&
+        self.subscription_id == other.subscription_id &&
+        self.refund_created.unix_timestamp() == other.refund_created.unix_timestamp() &&
+        self.refund_amount == other.refund_amount &&
+        self.refund_status == other.refund_status &&
+        self.refund_reason == other.refund_reason &&
+        self.status == other.status
+        // Compare manually if needed
+        // self.status_history == other.status_history
+        ;
+        let status_t_match = match self.status_t {
+            Some(self_v) => match other.status_t {
+                Some(other_v) => self_v.unix_timestamp() == other_v.unix_timestamp(),
+                None => false,
+            },
+            None => other.status_t.is_none(),
+        };
+        status_t_match && simple_match
+    }
+}
+impl Eq for Refund {}
+
+impl UpdateStatus for Refund {
+    fn get_raw_status(&self) -> Option<String> {
+        self.status.clone()
+    }
+
+    fn get_raw_status_history(&self) -> Option<JsonValue> {
+        self.status_history.clone()
+    }
+
+    fn set_raw_status(&mut self, v: Option<String>) {
+        self.status = v;
+    }
+
+    fn set_raw_status_history(&mut self, v: Option<JsonValue>) {
+        self.status_history = v;
+    }
+
+    fn get_status_t(&self) -> Option<OffsetDateTime> {
+        self.status_t
+    }
+
+    fn set_status_t(&mut self, v: Option<OffsetDateTime>) {
+        self.status_t = v;
+    }
+}
+
+impl Refund {
+    pub fn new(partial: PartialRefund) -> Self {
+        let mut r = Refund {
+            id: partial.id,
+            refund_id: partial.refund_id,
+            subscription_id: partial.subscription_id,
+            refund_created: partial.refund_created,
+            refund_amount: partial.refund_amount,
+            refund_status: partial.refund_status,
+            refund_reason: partial.refund_reason,
+            status: None,
+            status_t: None,
+            status_history: None,
+        };
+        r.update_status(Status::NotReported);
+        r
+    }
 }
 
 pub struct RefundModel<'a> {
@@ -43,5 +124,11 @@ impl RefundModel<'_> {
         )
         .fetch_one(self.db_pool)
         .await
+    }
+
+    pub async fn fetch_one_by_id(&self, id: &Uuid) -> Result<Refund, Error> {
+        query_as!(Refund, "SELECT * FROM refunds WHERE id = $1", id)
+            .fetch_one(self.db_pool)
+            .await
     }
 }
--- tests/integration/models/refunds.rs
+++ tests/integration/models/refunds.rs
@@ -1,1 +1,37 @@
+use crate::utils::{get_test_db_pool, random_price, random_simple_ascii_string};
+use lib::models::refunds::{PartialRefund, Refund, RefundModel};
+use pretty_assertions::assert_eq;
+use time::OffsetDateTime;
+use uuid::Uuid;
 
+pub fn make_fake_refund() -> Refund {
+    Refund::new(PartialRefund {
+        id: Uuid::new_v4(),
+        subscription_id: random_simple_ascii_string(),
+        refund_id: random_simple_ascii_string(),
+        refund_created: OffsetDateTime::now_utc(),
+        refund_amount: random_price(),
+        refund_status: Some(random_simple_ascii_string()),
+        refund_reason: Some(random_simple_ascii_string()),
+    })
+}
+
+pub async fn save_refund(model: &RefundModel<'_>, refund: &Refund) {
+    model
+        .create_from_refund(refund)
+        .await
+        .expect("Failed to create test object.");
+}
+
+#[tokio::test]
+async fn test_refund_model_create_from_refund_struct() {
+    let db_pool = get_test_db_pool().await;
+    let model = RefundModel { db_pool: &db_pool };
+    let r = make_fake_refund();
+    save_refund(&model, &r).await;
+    let result = model
+        .fetch_one_by_id(&r.id)
+        .await
+        .expect("Could not fetch from DB.");
+    assert_eq!(result, r);
+}
