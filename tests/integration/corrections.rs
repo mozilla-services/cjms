3a4b670a8ccd3bf9dbdf08bc595bf77963ab82cc
Sarah Bird <birdsarah@mozilla.com>
2022-03-29 16:28:32 -0500

    Code as far as sending basic auth from settings

--- tests/integration/corrections.rs
+++ tests/integration/corrections.rs
@@ -12,8 +12,14 @@ async fn test_corrections_get_requires_basic_auth() {
             "Expected Basic Auth to be required for {}",
             path
         );
-        // TODO - send basic auth with header
-        let r = send_get_request(&app, path).await;
+        let path = app.build_url(path);
+        let client = reqwest::Client::new();
+        let r = client
+            .get(&path)
+            .basic_auth("corrections", app.settings.cj_auth)
+            .send()
+            .await
+            .expect("Failed to GET");
         assert_eq!(r.status(), 200, "Could not access {}.", path);
     }
 }
