c6be8ed423b5f6a6bacd1d66bffba8ab93b99182
Sarah Bird <birdsarah@mozilla.com>
2022-04-02 17:03:47 -0500

    Simplify path code

--- src/lib/controllers/corrections.rs
+++ src/lib/controllers/corrections.rs
@@ -1,20 +1,14 @@
 use actix_web::{web, HttpResponse};
-use serde::Deserialize;
 use sqlx::PgPool;
 
 use crate::settings::Settings;
 
-#[derive(Deserialize)]
-pub struct CorrectionsPath {
-    id: String,
-}
-
 pub async fn detail(
-    path: web::Path<CorrectionsPath>,
+    path: web::Path<String>,
     settings: web::Data<Settings>,
     _pool: web::Data<PgPool>,
 ) -> HttpResponse {
-    if !path.id.eq(&settings.authentication) {
+    if !path.into_inner().eq(&settings.authentication) {
         return HttpResponse::NotFound().finish();
     }
     HttpResponse::Ok().finish()
