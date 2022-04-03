27f0deebd586948ee41acbe42a8d2340569261bd
Sarah Bird <birdsarah@mozilla.com>
2022-04-03 15:57:07 -0500

    Add some initial tests for corrections file

--- src/lib/controllers/corrections.rs
+++ src/lib/controllers/corrections.rs
@@ -1,11 +1,68 @@
 use actix_web::{web, HttpResponse};
+use serde::Deserialize;
 use sqlx::PgPool;
+use time::Date;
+
+use crate::{
+    models::refunds::{Refund, RefundModel},
+    settings::Settings,
+};
+
+fn build_body_from_results(settings: &Settings, results: Vec<Refund>) -> String {
+    let mut body = format!(
+        r#"
+&CID={}
+&SUBID=123"#,
+        settings.cj_cid
+    );
+    for refund in results {
+        body.push_str(&format!(
+            r#"
+RETRN,,{}"#,
+            refund.subscription_id
+        ));
+    }
+    body
+}
+
+async fn get_results_for_day(db_pool: &PgPool, day: Date) -> Vec<Refund> {
+    let refunds = RefundModel { db_pool };
+    // Intentional panic, can't continue if can't get refunds for today
+    refunds
+        .fetch_by_correction_file_day(&day)
+        .await
+        .unwrap_or_else(|_| panic!("Could not fetch refunds for date: {}", day))
+}
 
 pub async fn today(_pool: web::Data<PgPool>) -> HttpResponse {
     HttpResponse::Ok().finish()
 }
 
-pub async fn by_day(path: web::Path<String>, _pool: web::Data<PgPool>) -> HttpResponse {
-    let today = path.into_inner();
-    HttpResponse::Ok().body(today)
+#[derive(Deserialize)]
+pub struct CorrectionsByDayPath {
+    #[serde(with = "date_parser")]
+    day: Date,
+}
+
+mod date_parser {
+    use serde::{self, Deserialize, Deserializer};
+    use time::Date;
+    const FORMAT: &str = "%F";
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Date, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        Date::parse(s, FORMAT).map_err(serde::de::Error::custom)
+    }
+}
+
+pub async fn by_day(
+    path: web::Path<CorrectionsByDayPath>,
+    pool: web::Data<PgPool>,
+    settings: web::Data<Settings>,
+) -> HttpResponse {
+    let results = get_results_for_day(pool.as_ref(), path.day).await;
+    let body = build_body_from_results(settings.as_ref(), results);
+    HttpResponse::Ok().body(body)
 }
--- tests/integration/corrections.rs
+++ tests/integration/corrections.rs
@@ -1,4 +1,31 @@
-use crate::utils::spawn_app;
+use lib::models::refunds::RefundModel;
+use reqwest::Response;
+use time::{date, Date, OffsetDateTime};
+
+use crate::{
+    models::refunds::{make_fake_refund, save_refund},
+    utils::spawn_app,
+};
+
+const ANOTHER_DAY: Date = date!(2021 - 11 - 07);
+
+async fn make_test_refunds(model: &RefundModel<'_>) {
+    let today = OffsetDateTime::now_utc().date();
+    let mut refund_1 = make_fake_refund();
+    refund_1.refund_id = "refund_1".to_string();
+    refund_1.correction_file_date = Some(today);
+    let mut refund_2 = make_fake_refund();
+    refund_2.refund_id = "refund_2".to_string();
+    refund_2.correction_file_date = Some(ANOTHER_DAY);
+    let mut refund_3 = make_fake_refund();
+    refund_3.correction_file_date = Some(today);
+    refund_3.refund_id = "refund_3".to_string();
+    let mut refund_4 = make_fake_refund();
+    refund_4.correction_file_date = None;
+    for r in [&refund_1, &refund_2, &refund_3, &refund_4] {
+        save_refund(model, r).await;
+    }
+}
 
 #[tokio::test]
 async fn test_corrections_today_path() {
@@ -46,5 +73,54 @@ async fn test_corrections_by_day_auth() {
         .await
         .expect("Failed to GET");
     assert_eq!(r.status(), 200);
-    assert_eq!(r.text().await.unwrap(), "20220328");
+}
+
+async fn get_authed_path(path: &str, password: &str) -> Response {
+    let client = reqwest::Client::new();
+    let r = client
+        .get(path)
+        .basic_auth("user", Some(password))
+        .send()
+        .await
+        .expect("Failed to GET");
+    r
+}
+
+#[tokio::test]
+async fn test_corrections_by_day_result() {
+    let app = spawn_app().await;
+    let refunds = RefundModel {
+        db_pool: &app.db_connection(),
+    };
+    make_test_refunds(&refunds).await;
+    let expected_refund = refunds.fetch_one_by_refund_id("refund_2").await.unwrap();
+
+    // Path with no expected refund
+    let path = app.build_url("/corrections/2020-01-01.csv");
+    let r = get_authed_path(&path, &app.settings.authentication).await;
+    assert_eq!(r.status(), 200);
+    let actual_body = r.text().await.unwrap();
+    // TODO - SUBID Needs to BE IN SETTINGS
+    let expected_body = format!(
+        r#"
+&CID={}
+&SUBID=123"#,
+        app.settings.cj_cid
+    );
+    assert_eq!(actual_body.trim(), expected_body.trim());
+
+    // Path with expected refund
+    let path = app.build_url("/corrections/2021-11-07.csv");
+    let r = get_authed_path(&path, &app.settings.authentication).await;
+    assert_eq!(r.status(), 200);
+    let actual_body = r.text().await.unwrap();
+    // TODO - This is wrong it should be OID which is refund.subscription.id
+    let expected_body = format!(
+        r#"
+&CID={}
+&SUBID=123
+RETRN,,{}"#,
+        app.settings.cj_cid, expected_refund.subscription_id
+    );
+    assert_eq!(actual_body.trim(), expected_body.trim());
 }
