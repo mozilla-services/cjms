dfe4a4498581f4016e4ec67e0b90f329920df500
Sarah Bird <birdsarah@mozilla.com>
2022-04-05 16:47:14 -0500

    Complete clean-up

--- src/lib/jobs/cleanup.rs
+++ src/lib/jobs/cleanup.rs
@@ -1,3 +1,26 @@
 use sqlx::PgPool;
 
-pub async fn archive_expired_aics(_db_pool: &PgPool) {}
+use crate::models::aic::AICModel;
+
+pub async fn archive_expired_aics(db_pool: &PgPool) {
+    let aic_model = AICModel { db_pool };
+    // Intentional expect. Cannot continue without
+    let expired = aic_model
+        .fetch_expired()
+        .await
+        .expect("Could not get expired AICs");
+    for aic in expired {
+        match aic_model.archive_aic(&aic).await {
+            Ok(_) => {
+                println!("Successfully archived aic: {}", &aic.id);
+            }
+            Err(e) => {
+                println!(
+                    "Could not archive aic: {}. Error: {}. Continuing...",
+                    &aic.id, e
+                );
+                continue;
+            }
+        }
+    }
+}
--- tests/integration/jobs/cleanup.rs
+++ tests/integration/jobs/cleanup.rs
@@ -14,24 +14,40 @@ async fn test_archive_expired_aics() {
     // Should be archived
     let mut aic_1 = make_fake_aic();
     aic_1.expires = now - Duration::seconds(5);
+    // Add one that's already archived to check we can handle errors
+    let mut aic_bad = make_fake_aic();
+    aic_bad.expires = now - Duration::seconds(5);
     // Should not be archived
     let mut aic_2 = make_fake_aic();
     aic_2.expires = now + Duration::seconds(5);
     // Should be archived
     let mut aic_3 = make_fake_aic();
     aic_3.expires = now - Duration::seconds(5);
-    for aic in [&aic_1, &aic_2, &aic_3] {
+    for aic in [&aic_1, &aic_2, &aic_3, &aic_bad] {
         aic_model
             .create_from_aic(aic)
             .await
             .expect("Could not create AIC");
     }
+    aic_model
+        .create_archive_from_aic(&aic_bad)
+        .await
+        .expect("Could not create pre-archived AIC.");
 
     archive_expired_aics(&db_pool).await;
 
-    assert!(aic_model.fetch_one_by_id_from_archive(&aic_1.id).await.is_ok());
-    assert!(aic_model.fetch_one_by_id_from_archive(&aic_2.id).await.is_err());
-    assert!(aic_model.fetch_one_by_id_from_archive(&aic_3.id).await.is_ok());
+    assert!(aic_model
+        .fetch_one_by_id_from_archive(&aic_1.id)
+        .await
+        .is_ok());
+    assert!(aic_model
+        .fetch_one_by_id_from_archive(&aic_2.id)
+        .await
+        .is_err());
+    assert!(aic_model
+        .fetch_one_by_id_from_archive(&aic_3.id)
+        .await
+        .is_ok());
 
     assert!(aic_model.fetch_one_by_id(&aic_1.id).await.is_err());
     assert!(aic_model.fetch_one_by_id(&aic_2.id).await.is_ok());
