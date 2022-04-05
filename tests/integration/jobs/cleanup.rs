fca1ee02d98c3db3ad71560a30bef0ef5ce7d567
Sarah Bird <birdsarah@mozilla.com>
2022-04-05 16:29:18 -0500

    Add failing test

--- tests/integration/jobs/cleanup.rs
+++ tests/integration/jobs/cleanup.rs
@@ -1,10 +1,39 @@
+use lib::jobs::cleanup::archive_expired_aics;
 use lib::models::aic::AICModel;
+use time::{Duration, OffsetDateTime};
 
-use crate::utils::get_test_db_pool;
+use crate::{models::aic::make_fake_aic, utils::get_test_db_pool};
 
 #[tokio::test]
-async fn archive_expired_aics() {
+async fn test_archive_expired_aics() {
     // SETUP
     let db_pool = get_test_db_pool().await;
-    let _aics = AICModel { db_pool: &db_pool };
+    let aic_model = AICModel { db_pool: &db_pool };
+
+    let now = OffsetDateTime::now_utc();
+    // Should be archived
+    let mut aic_1 = make_fake_aic();
+    aic_1.expires = now - Duration::seconds(5);
+    // Should not be archived
+    let mut aic_2 = make_fake_aic();
+    aic_2.expires = now + Duration::seconds(5);
+    // Should be archived
+    let mut aic_3 = make_fake_aic();
+    aic_3.expires = now - Duration::seconds(5);
+    for aic in [&aic_1, &aic_2, &aic_3] {
+        aic_model
+            .create_from_aic(aic)
+            .await
+            .expect("Could not create AIC");
+    }
+
+    archive_expired_aics(&db_pool).await;
+
+    assert!(aic_model.fetch_one_by_id_from_archive(&aic_1.id).await.is_ok());
+    assert!(aic_model.fetch_one_by_id_from_archive(&aic_2.id).await.is_err());
+    assert!(aic_model.fetch_one_by_id_from_archive(&aic_3.id).await.is_ok());
+
+    assert!(aic_model.fetch_one_by_id(&aic_1.id).await.is_err());
+    assert!(aic_model.fetch_one_by_id(&aic_2.id).await.is_ok());
+    assert!(aic_model.fetch_one_by_id(&aic_3.id).await.is_err());
 }
