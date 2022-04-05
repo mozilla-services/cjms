41e8a7db7ce55ba443f0623f5e916619ddde50d1
Jeremy Swinarton <jswinarton@mozilla.com>
2022-04-05 17:49:24 -0400

    more

--- src/bin/web.rs
+++ src/bin/web.rs
@@ -5,6 +5,10 @@ use lib::{
 };
 use std::net::TcpListener;
 
+use cadence::prelude::*;
+use cadence::{StatsdClient, UdpMetricSink};
+use std::net::UdpSocket;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let settings = get_settings();
@@ -15,6 +19,16 @@ async fn main() -> std::io::Result<()> {
     // Statsd basic integration
     // TODO move into its own file
     // TODO investigate non-blocking version
+    let host = (
+        settings.statsd_host.clone(),
+        settings.statsd_port.parse::<u16>().unwrap(),
+    );
+    let socket = UdpSocket::bind("0.0.0.0:0").unwrap();
+    let sink = UdpMetricSink::from(host, socket).unwrap();
+    let client = StatsdClient::from_sink("cjms-web", sink);
+
+    // TODO what to do with the error?
+    client.incr("app-init").unwrap();
 
     let addr = settings.server_address();
     let db_pool = connect_to_database_and_migrate(&settings.database_url).await;
