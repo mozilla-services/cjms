3971179125b34b2e8faf23b2e454baaba5a16840
Jeremy Swinarton <jswinarton@mozilla.com>
2022-04-06 12:49:09 -0400

    more

--- src/bin/cleanup.rs
+++ src/bin/cleanup.rs
@@ -1,11 +1,17 @@
 use lib::{
-    appconfig::connect_to_database_and_migrate, jobs::cleanup::archive_expired_aics,
+    appconfig::connect_to_database_and_migrate,
+    jobs::cleanup::archive_expired_aics,
     settings::get_settings,
+    telemetry::{init_sentry, init_tracing},
 };
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     let settings = get_settings();
+
+    let _guard = init_sentry(&settings);
+    init_tracing("cjms-cleanup", &settings.log_level, std::io::stdout);
+
     let db = connect_to_database_and_migrate(&settings.database_url).await;
     // TODO - LOGGING - This is a process we'll want to log and time (if possible)
     println!("Starting cleanup");
