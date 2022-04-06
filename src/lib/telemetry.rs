90c4028d8e89277d433f3cc3f99754c13e4c0d22
Jeremy Swinarton <jswinarton@mozilla.com>
2022-04-06 13:24:18 -0400

    more

--- src/lib/telemetry.rs
+++ src/lib/telemetry.rs
@@ -1,4 +1,4 @@
-use cadence::{StatsdClient, UdpMetricSink};
+&&use cadence::{StatsdClient, UdpMetricSink};
 use sentry::ClientInitGuard;
 use sentry_tracing::EventFilter;
 use std::borrow::Cow;
@@ -11,6 +11,20 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 
 use crate::settings::Settings;
 use crate::version::{read_version, VERSION_FILE};
+
+pub enum TraceType {
+    AicRecordCreate,
+    AicRecordCreateFailed,
+}
+
+impl TraceType {
+    fn as_str(&self) -> &'static str {
+        match self {
+            TraceType::AicRecordCreate => "aic-record-create",
+            TraceType::AicRecordCreateFailed => "aic-record-create-failed",
+        }
+    }
+}
 
 /// Creates a tracing subscriber and sets it as the global default.
 pub fn init_tracing<Sink>(service_name: &str, log_level: &str, sink: Sink)
@@ -46,13 +60,15 @@ pub fn init_sentry(settings: &Settings) -> ClientInitGuard {
         settings.sentry_dsn.clone(),
         sentry::ClientOptions {
             environment: Some(Cow::from(settings.environment.clone())),
+            // Suppress breadcrumbs.
+            max_breadcrumbs: 0,
             release: Some(Cow::from(version_data.version)),
-            /// `sample_rate` defines the sample rate of error events (i.e. panics and error
-            /// log messages). Should always be 1.0.
+            // `sample_rate` defines the sample rate of error events (i.e. panics and error
+            // log messages). Should always be 1.0.
             sample_rate: 1.0,
-            /// `traces_sample_rate` defines the sample rate of "transactional"
-            /// events that are used for performance insights. We don't want any
-            /// of this, so we set to zero.
+            // `traces_sample_rate` defines the sample rate of "transactional"
+            // events that are used for performance insights. We don't want any
+            // of this, so we set to zero.
             traces_sample_rate: 0.0,
             ..Default::default()
         },
@@ -69,4 +85,28 @@ pub fn create_statsd_client(settings: &Settings) -> StatsdClient {
     let sink = UdpMetricSink::from(host, socket).unwrap();
 
     StatsdClient::from_sink("cjms", sink)
+}
+
+pub fn trace(trace_type: TraceType, message: &str) {
+    tracing::trace!(r#type = trace_type.to_string(), message);
+}
+
+pub fn debug(trace_type: TraceType, message: &str) {
+    tracing::debug!(r#type = trace_type.to_string(), message);
+}
+
+pub fn info(trace_type: TraceType, message: &str) {
+    tracing::info!(r#type = trace_type.to_string(), message);
+}
+
+pub fn warn(trace_type: TraceType, message: &str) {
+    tracing::warn!(r#type = trace_type.to_string(), message);
+}
+
+pub fn error(trace_type: TraceType, message: &str, error: Option<Box<dyn std::error::Error>>) {
+    let message = match error {
+        Some(err) => format!("Message: '{}'. Original error: {}", &message, err),
+        None => message,
+    };
+    tracing::error!(r#type = trace_type.to_string(), message);
 }
