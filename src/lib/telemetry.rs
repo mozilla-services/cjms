d987764c9c27f1d0b45a5dca35c84ce07aff643c
Jeremy Swinarton <jswinarton@mozilla.com>
2022-05-11 10:54:20 -0400

    Refactor add_suffix (#69)

--- src/lib/telemetry.rs
+++ src/lib/telemetry.rs
@@ -18,7 +18,7 @@ use tracing_subscriber::{layer::SubscriberExt, EnvFilter, Registry};
 use crate::settings::Settings;
 use crate::version::{read_version, VERSION_FILE};
 
-#[derive(Debug, EnumToString, EnumString, PartialEq, Eq)]
+#[derive(Debug, EnumToString, EnumString, PartialEq, Eq, Clone, Copy)]
 #[strum(serialize_all = "kebab_case")]
 pub enum LogKey {
     AicRecordCreate,
@@ -136,12 +136,11 @@ pub enum LogKey {
 
 impl LogKey {
     pub fn add_suffix(&self, suffix: &str) -> LogKey {
-        let s = format!("{}-{}", &self.to_string(), suffix);
-        let s_str = s.as_str();
+        let s = self.to_string() + "-" + suffix;
 
-        match LogKey::from_str(s_str) {
+        match LogKey::from_str(&s) {
             Ok(v) => v,
-            Err(_) => LogKey::from_str(&self.to_string()).unwrap(),
+            Err(_) => *self,
         }
     }
 }
