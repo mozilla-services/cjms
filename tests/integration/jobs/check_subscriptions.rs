0df512de1e9b17cc7e8817a27ab27b276f65eaa9
Sarah Bird <birdsarah@mozilla.com>
2022-05-27 16:30:20 -0500

    Add failing test for check_subscriptions

--- tests/integration/jobs/check_subscriptions.rs
+++ tests/integration/jobs/check_subscriptions.rs
@@ -48,6 +48,8 @@ async fn check_subscriptions() {
     let sub_no_aic_entry_flow_id = "not-in-the-aic_tables";
     // Happy path 2 at the end of all the other tests to ensure we're continuing correctly
     let sub_happy_2_flow_id = "6d8c011f70525c1d04aaa9813f93a3cdfc7316b95cdc172c48b1d6b7a522d338";
+    // Happy path with coupons
+    let sub_coupons_flow_id = "5a0d6f9f5ce977481a1ca72cadb64e5fbac48efea347f82bbf5788e1b9b920de";
 
     let mut aic_1 = make_fake_aic();
     aic_1.flow_id = sub_happy_flow_id.to_string();
@@ -57,8 +59,10 @@ async fn check_subscriptions() {
     aic_3.flow_id = sub_empty_string_flow_id.to_string();
     let mut aic_4 = make_fake_aic();
     aic_4.flow_id = sub_happy_2_flow_id.to_string();
+    let mut aic_5 = make_fake_aic();
+    aic_5.flow_id = sub_coupons_flow_id.to_string();
 
-    for aic in [&aic_1, &aic_2, &aic_3, &aic_4] {
+    for aic in [&aic_1, &aic_2, &aic_3, &aic_4, &aic_5] {
         aic_model
             .create_from_aic(aic)
             .await
@@ -97,8 +101,12 @@ async fn check_subscriptions() {
         .fetch_one_by_flow_id(sub_happy_2_flow_id)
         .await
         .expect("Failed to get sub 3");
+    let sub_4 = sub_model
+        .fetch_one_by_flow_id(sub_coupons_flow_id)
+        .await
+        .expect("Failed to get sub 4");
 
-    for sub in [&sub_1, &sub_2, &sub_3] {
+    for sub in [&sub_1, &sub_2, &sub_3, &sub_4] {
         // Expect all aics to be in archive now
         match aic_model.fetch_one_by_flow_id(&sub.flow_id).await {
             Err(sqlx::Error::RowNotFound) => {}
@@ -167,7 +175,7 @@ async fn check_subscriptions() {
         })
     );
     assert_eq!(
-        // Sub three is the last one so all the failure cases in the test fixture should have been handled if 2 is also created.
+        // Sub three is after all the failure cases in the test fixture should have been handled if 2 is also created.
         // TODO - LOGGING - when we add logging we could test for those logs to have been created
         sub_3,
         Subscription::new(PartialSubscription {
@@ -190,6 +198,31 @@ async fn check_subscriptions() {
             aic_id: Some(aic_4.id),
             aic_expires: Some(aic_4.expires),
             cj_event_value: Some(aic_4.cj_event_value),
+        })
+    );
+    assert_eq!(
+        // Sub four has a coupon code
+        sub_4,
+        Subscription::new(PartialSubscription {
+            id: sub_4.id, // We can't know this ahead of time
+            flow_id: sub_coupons_flow_id.to_string(),
+            subscription_id: "sub_1L47CjKb9q6OnNsLz4ekpzfv".to_string(),
+            report_timestamp: date!(2022 - 03 - 16)
+                .with_time(time!(20:59:53))
+                .assume_utc(),
+            subscription_created: date!(2022 - 03 - 16)
+                .with_time(time!(16:59:41))
+                .assume_utc(),
+            fxa_uid: "f71b0f8ef98e6d1f6a6b7ce533d58298b2e96b7db53bed198a5ff7ba6f4cda9d".to_string(),
+            quantity: 1,
+            plan_id: "price_1J0Y1iKb9q6OnNsLXwdOFgDr".to_string(),
+            plan_currency: "usd".to_string(),
+            plan_amount: 5389,
+            country: Some("us".to_string()),
+            coupons: Some("VPN10".to_string()),
+            aic_id: Some(aic_5.id),
+            aic_expires: Some(aic_5.expires),
+            cj_event_value: Some(aic_5.cj_event_value),
         })
     );
     // Expect to NOT have certain entries from the test fixtures
